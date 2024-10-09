f81974273ce488950ab2aba4afd0492dbd1374ea
Silvino Padilha Silva Nogueira <silvinopsn@dinfor.uniceub>
2024-10-09 20:21:14 -0300

    tabela personalizavel

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import './App.css'
 import Table from './components/table'
 
 function App() {
-	
+
 	return (
 		<>
-			<Table />
+			<Table categoria='Mesas' corCategoria='text-blue-500' corBorda='border-blue-400' />
+			<Table categoria='Pranchas' corCategoria='text-violet-500' corBorda='border-violet-400'/>
 		</>
 	)
 }
--- src/components/table.tsx
+++ src/components/table.tsx
@@ -1,49 +1,54 @@
+interface TableProps {
+	categoria: string,
+	corCategoria: string
+	corBorda: string
+}
 
-const Table = () => {
-  const rows = [
-    { name: 'Mesa de 1,20m', stock: 20, rented: 0, price: 'R$ 16,00' },
-    { name: 'Mesa de 1,30m', stock: 6, rented: 0, price: 'R$ 18,00' },
-    { name: 'Mesa de 1,40m', stock: 16, rented: 0, price: 'R$ 18,00' },
-    { name: 'Mesa de 1,50m', stock: 6, rented: 0, price: 'R$ 20,00' },
-  ];
+export function Table({ categoria, corCategoria, corBorda}: TableProps) {
+	const rows = [
+		{ name: 'Mesa de 1,20m', stock: 20, rented: 0, price: 'R$ 16,00' },
+		{ name: 'Mesa de 1,30m', stock: 6, rented: 0, price: 'R$ 18,00' },
+		{ name: 'Mesa de 1,40m', stock: 16, rented: 0, price: 'R$ 18,00' },
+		{ name: 'Mesa de 1,50m', stock: 6, rented: 0, price: 'R$ 20,00' },
+	];
 
-  return (
-    <div className="p-4">
-      <table className="w-full text-left border-separate border-spacing-y-2">
-        <thead>
-          <tr className="text-sm font-medium text-gray-500">
-            <th className="text-2xl font-semibold text-blue-500 mb-4">Mesas</th>
-            <th className="p-2">Quantidade no estoque</th>
-            <th className="p-2">Quantidade em locação</th>
-            <th className="p-2">Preço unitário</th>
-            <th className="p-2">Editar</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rows.map((row, index) => (
-            <tr key={index} className="bg-gray-50">
-              <td className="border-l-4 border-blue-400 p-2">{row.name}</td>
-              <td className="p-2">{row.stock}</td>
-              <td className="p-2">{row.rented}</td>
-              <td className="p-2">{row.price}</td>
-              <td className="p-2">
-              <button className="p-2 bg-gray-200 rounded">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  > 
-                    <path d="M17.414 2.586a2 2 0 010 2.828l-10 10A2 2 0 016 16H3v-3a2 2 0 01.586-1.414l10-10a2 2 0 012.828 0zM7 15v2h2l9.293-9.293-2-2L7 15z" />
-                  </svg>
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+	return (
+		<div className="p-4">
+			<table className="w-full text-left border-separate border-spacing-y-2">
+				<thead>
+					<tr className="text-sm font-medium text-gray-500">
+						<th className={`text-2xl font-semibold ${corCategoria} mb-4`}>{categoria}</th>
+						<th className="p-2">Quantidade no estoque</th>
+						<th className="p-2">Quantidade em locação</th>
+						<th className="p-2">Preço unitário</th>
+						<th className="p-2">Editar</th>
+					</tr>
+				</thead>
+				<tbody>
+					{rows.map((row, index) => (
+						<tr key={index} className="bg-gray-50">
+							<td className={`border-l-4 ${corBorda} p-2`}>{row.name}</td>
+							<td className="p-2">{row.stock}</td>
+							<td className="p-2">{row.rented}</td>
+							<td className="p-2">{row.price}</td>
+							<td className="p-2">
+								<button className="p-2 bg-gray-200 rounded">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										className="h-5 w-5"
+										viewBox="0 0 20 20"
+										fill="currentColor"
+									>
+										<path d="M17.414 2.586a2 2 0 010 2.828l-10 10A2 2 0 016 16H3v-3a2 2 0 01.586-1.414l10-10a2 2 0 012.828 0zM7 15v2h2l9.293-9.293-2-2L7 15z" />
+									</svg>
+								</button>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
 };
 
 export default Table;
